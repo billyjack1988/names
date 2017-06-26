6ebf8a51844972c2be1c57b784c3e3063d2793c1
billyjack1988 <billyjacktattoos@gmail.com>
2017-06-26 11:57:46 -0500

    my_name_is

--- billyjack.rb
+++ billyjack.rb
@@ -1,3 +1,21 @@
-puts "Billy"
-p "Jack"
-print "Buzzard"
+
+puts "What is your name?"
+
+first_name = gets.chomp.to_s
+
+case (first_name)
+when "Billy"
+	puts " Billy or Billyjack"
+	when "BillyJack"
+		puts "ok Thank You."
+	end
+
+puts "whats your last name?"
+
+last_name = gets.chomp.to_s
+case (last_name)
+when "Buzzard"
+	puts " Thank You. BillyJack Buzzard"
+end   
+
+
